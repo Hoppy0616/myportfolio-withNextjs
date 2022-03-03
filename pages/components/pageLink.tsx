a56368009800fde6ef7f0ee48da4d0a06899fb99
Hoppy0616 <hodahoda.20430@gmail.com>
2022-03-04 00:45:22 +0900

    ファイル名の修正、一部div化

--- pages/components/pageLink.tsx
+++ pages/components/pageLink.tsx
@@ -1,12 +1,13 @@
 import { type } from "os";
 import Link from "next/link";
+import styles from "../../styles/components/pageLink.module.scss"
 type Props = {
   title: string;
 };
 
 const pageLink = (props: Props) => {
   return (
-    <div>
+    <div className={styles.pageLink}>
       <Link href={props.title}>
         <a>{props.title}</a>
       </Link>
